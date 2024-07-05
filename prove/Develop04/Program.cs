83e7137f8fd4c473a3e76f60e64580c823bfb920
preye45 <preyetawariowei@gmail.com>
2024-07-05 21:47:47 +0100

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+         Activity asset = new Activity();
+        Breathing breathing = new Breathing();
+        Reflection reflection = new Reflection();
+        Listing listing = new Listing();
+        Console.WriteLine("\n Welcome To The Mindfulness Activity \n");
+        Console.WriteLine("\n(Select the Activity that you want by it number)\n");
+        Console.Clear();
+        int userChoice;
+        
+        do
+        {
+          Console.WriteLine("1. Start Breathing Activity 🫁 | 2. Start Refelecting Activity 🧘🏽‍♀️ | 3. Start Listing Activity 📝 | 4. Quit ❌\n");
+          Console.Write("Select your activity: ");
+          userChoice = int.Parse(Console.ReadLine()); 
+          Console.WriteLine("");
+
+          if (userChoice == 1)
+          {
+            breathing.Run();
+            Console.WriteLine("");
+          }
+          else if(userChoice == 2)
+          {
+            reflection.Run();
+          }
+          else if (userChoice == 3)
+          {
+            listing.Run();
+          }
+
+        } while (userChoice != 4);
+        
     }
 }
