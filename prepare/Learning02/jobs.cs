f6b96bbd008c59fe0cf0ac5257b8200f447c8769
preye45 <preyetawariowei@gmail.com>
2024-07-05 07:33:31 +0100

    Create jobs.cs

---
+++ prepare/Learning02/jobs.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Job job1 = new Job();
+        job1._jobTitle = "Data Analyst";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mike Tyson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }
+}
